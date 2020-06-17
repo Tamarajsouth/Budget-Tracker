bdb2f42666a792611db4819962367bae20aded8f
tamara south <tjeansouth@gmail.com>
2020-06-17 14:43:13 -0400

    added heroku mLab code to server.js

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+// if deployed, use the deployed database. Otherwise, use the local mongoHeadlines
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
